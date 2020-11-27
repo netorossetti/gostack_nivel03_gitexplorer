4fd24c7943eb5869bdbed85070e59c122aae2d61
Neto Rossetti <netinho.rossetti@gmail.com>
2020-11-27 15:40:30 -0300

    commit aula 9: Estilização da pagina de detalhes do repositório.

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,15 +1,64 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Header, RepositoryInfo, Issues } from './style';
 
 interface RepositoryParms {
     repository: string;
 }
 
-const Repository: React.FC = () =>{
+const Repository: React.FC = () => {
 
     const { params } = useRouteMatch<RepositoryParms>();
 
-    return <h1>Repository:{params.repository}</h1>
-};
+    return (
+        <>
+            <Header>
+                <img src={logoImg} alt="Github Explorer" />
+                <Link to="/">
+                    <FiChevronLeft size={16} />
+                    Voltar
+                </Link>
+            </Header>
+
+            <RepositoryInfo>
+                <header>
+                    <img src="https://avatars0.githubusercontent.com/u/4626113?s=460&u=81dbf8de7266957c288e3fe969e054bfb8ca3e25&v=4" alt="imagem" />
+                    <div>
+                        <strong>Titulo Repo</strong>
+                        <p>Descrição Repo</p>
+                    </div>
+                </header>
+                <ul>
+                    <li>
+                        <strong>1808</strong>
+                        <span>Stars</span>
+                    </li>
+                    <li>
+                        <strong>48</strong>
+                        <span>Forks</span>
+                    </li>
+                    <li>
+                        <strong>67</strong>
+                        <span>Issues abertas</span>
+                    </li>
+                </ul>
+            </RepositoryInfo>
+
+            <Issues>
+                <Link to="vddada">
+                    <div>
+                        <strong>repository.full_name</strong>
+                        <p>repository.description</p>
+                    </div>
+                    <FiChevronRight size={20} />
+                </Link>
+            </Issues>
+        </>
+    );
+}
 
 export default Repository;
